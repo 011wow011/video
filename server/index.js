39b800a01d7b9558a02bb74dc649027917e97819
DESKTOP-7MV8AF5\Snooks <011wow011@naver.com>
2022-10-05 03:11:20 +0900

    영상 공유 앱

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import userRoutes from "./routes/users.js"
+import videoRoutes from "./routes/comments.js"
+import commentRoutes from "./routes/videos.js"
+import authRoutes from "./routes/auth.js"
+
+const app = express()
+dotenv.config()
+
+const connect = () => {
+    mongoose
+    .connect(process.env.MONGO)
+    .then(()=> {
+        console.log("Connected to DB");
+    })
+    .catch((err)=>{
+        throw err;
+    });
+};
+
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
+
+app.listen(8800, ()=>{
+    connect()
+    console.log("Connected to Server");
+})
