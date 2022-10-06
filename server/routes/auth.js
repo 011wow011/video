754eca8e7a8101a66a655bf06f75f2e909723b1a
DESKTOP-7MV8AF5\Snooks <011wow011@naver.com>
2022-10-07 05:59:20 +0900

    서버

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,7 @@ import express from "express";
 import { signup, signin } from "../controllers/auth.js";
 const router = express.Router();
 
+
 //CREATE A USER
 router.post("/signup", signup)
 //SIGN IN
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,29 @@
 import express from "express";
-import { } from "../controllers/user.js";
-
+import {
+  update,
+  deleteUser,
+  getUser,
+  subscribe,
+  unsubscribe,
+  like,
+  dislike,
+} from "../controllers/user.js";
+import { verifyToken } from "../verifyToken.js";
 const router = express.Router();
+
+//유저 업데이트
+router.put("/:id", verifyToken, update);
+//유저 삭제
+router.delete("/:id", verifyToken, deleteUser);
+//유저 불러오기
+router.get("/find/:id", getUser);
+//구독
+router.put("/sub/:id", verifyToken, subscribe);
+//구독 취소
+router.put("/unsub/:id", verifyToken, unsubscribe);
+//좋아요
+router.put("/like/:videoId", verifyToken, like);
+//좋아요 취소
+router.put("/dislike/:videoId", verifyToken, dislike);
 
 export default router;
